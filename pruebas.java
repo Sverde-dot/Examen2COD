6b6664b8bdf8eb48f908d827f7ae8330e81ade96
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2020-03-11 13:33:22 +0100

    reenvio de datos
    fallo al lanzar los datos en el anterior commit solucionado

--- pruebas.java
+++ pruebas.java
@@ -10,5 +10,27 @@ package codexam;
  * @author sverdecadilla
  */
 public class pruebas {
+    public int Dato1=50;
+    public int Dato2=50;
+
+    public pruebas() {
+    }
+
+    public int getDato1() {
+        return Dato1;
+    }
+
+    public void setDato1(int Dato1) {
+        this.Dato1 = Dato1;
+    }
+
+    public int getDato2() {
+        return Dato2;
+    }
+
+    public void setDato2(int Dato2) {
+        this.Dato2 = Dato2;
+    }
+    
     
 }
